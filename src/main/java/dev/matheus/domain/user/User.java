e30656a1b1e498ed772926271922405b674ac733
Matheus Sousa <matheusss.dev01@gmail.com>
2024-10-14 19:53:47 -0300

    feat: create User entity

---
+++ src/main/java/dev/matheus/domain/user/User.java
@@ -0,0 +1,40 @@
+package dev.matheus.domain.user;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "tb_users")
+public class User extends PanacheEntityBase {
+
+    // Id do usuário no banco de dados
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    // Primeiro nome do usuário
+    public String firstName;
+
+    // Segundo nome do usuário
+    public String lastName;
+
+    // CPF do usuário com regra para cadastro único
+    @Column(unique = true)
+    public String  document;
+
+    // E-mail do usuário com regra para cadastro único
+    @Column(unique = true)
+    public String email;
+
+    // Senha do usuário
+    public String password;
+
+    // Saldo do usuário
+    public BigDecimal balance;
+
+    // Tipo de usuário
+    @Enumerated(EnumType.STRING)
+    public UserType userType;
+}
---
+++ src/main/java/dev/matheus/domain/user/UserType.java
@@ -0,0 +1,10 @@
+package dev.matheus.domain.user;
+
+public enum UserType {
+
+    // Usuários normais
+    COMMON,
+
+    // Logistas
+    MERCHANT
+}
